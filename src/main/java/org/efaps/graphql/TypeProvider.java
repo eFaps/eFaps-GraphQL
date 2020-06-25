09d3a10680a14887f7557c0a36dba69ddfc7a4da
Jan Moxter <jan.moxter@moxter.net>
2020-06-24 22:59:15 -0500

    TypeProvider

---
+++ src/main/java/org/efaps/graphql/TypeProvider.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2003 - 2020 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.efaps.graphql;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.efaps.eql.EQL;
+import org.efaps.eql.builder.Selectables;
+import org.efaps.graphql.ci.CIGraphQL;
+import org.efaps.util.EFapsException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import graphql.Scalars;
+import graphql.schema.GraphQLFieldDefinition;
+import graphql.schema.GraphQLObjectType;
+import graphql.schema.GraphQLType;
+
+public class TypeProvider
+{
+
+    private static final Logger LOG = LoggerFactory.getLogger(TypeProvider.class);
+
+    public Set<GraphQLType> getTypes()
+        throws EFapsException
+    {
+        final var ret = new HashSet<GraphQLType>();
+        final var eval = EQL.builder().print()
+                        .query(CIGraphQL.ObjectType)
+                        .select()
+                        .attribute(CIGraphQL.ObjectType.Name)
+                        .evaluate();
+        while (eval.next()) {
+            final String name = eval.get(CIGraphQL.ObjectType.Name);
+            LOG.info("Type: {}", name);
+            final var objectTypeBldr = GraphQLObjectType.newObject()
+                            .name(name);
+
+            final var fieldEval = EQL.builder().print()
+                            .query(CIGraphQL.FieldDefinition)
+                            .where()
+                            .attribute(CIGraphQL.FieldDefinition.ID)
+                            .in(EQL.builder()
+                                            .nestedQuery(CIGraphQL.ObjectType2FieldDefinition)
+                                            .where()
+                                            .attribute(CIGraphQL.ObjectType2FieldDefinition.FromID).eq(eval.inst())
+                                            .up()
+                                            .selectable(Selectables
+                                                            .attribute(CIGraphQL.ObjectType2FieldDefinition.ToID)))
+                            .select()
+                            .attribute(CIGraphQL.FieldDefinition.Name)
+                            .evaluate();
+
+            while (fieldEval.next()) {
+                final String fieldName = fieldEval.get(CIGraphQL.FieldDefinition.Name);
+                LOG.info("    Field: {}", fieldName);
+                objectTypeBldr.field(GraphQLFieldDefinition.newFieldDefinition()
+                                .name(fieldName)
+                                .type(Scalars.GraphQLString));
+            }
+            ret.add(objectTypeBldr.build());
+        }
+        return ret;
+    }
+
+}
